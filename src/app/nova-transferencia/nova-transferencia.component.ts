93df40eee95a09557eceb43ac5e32a9a81fa14cc
diegooliveyra <diegooliveirabryto@gmail.com>
2021-09-13 16:23:15 -0300

    :sparkles: feito a exportação dos dados do form

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent implements OnInit {
+export class NovaTransferenciaComponent {
+  valor!: number;
+  destino!: number;
 
-  constructor() { }
+  @Output() aotransferi = new EventEmitter<any>();
+  constructor() {}
 
-  ngOnInit() {
+  transferir(): void {
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aotransferi.emit(valorEmitir);
   }
-
 }
